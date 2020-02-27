25384355c7e9a485c5571546b087755d3253ad80
Darren Isaacson <yc8121yl@go.minneapolis.edu>
2020-02-27 14:54:51 -0600

    Added ICanHazDadjoke api connection

---
+++ api/connections.py
@@ -0,0 +1,18 @@
+import requests
+
+from pprint import pprint
+
+headers = {
+    'Accept': 'application/json',
+    'User-Agent': 'jokester:https://github.com/4LineTime/Jokester',
+    'Email': 'yc8121yl@minnstate.edu'  # This is another valid field
+}
+
+
+url = "https://icanhazdadjoke.com/"
+
+
+data = requests.get(url, headers=headers).json()
+
+
+pprint(data)
