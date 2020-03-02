d4d9ab06405b0cb4a4c1b6a76211454d9483a16e
Darren Isaacson <yc8121yl@go.minneapolis.edu>
2020-03-02 16:58:16 -0600

    Created the file and added some functions that could be useful

---
+++ configs/view_tools.py
@@ -0,0 +1,28 @@
+
+def make_selection(menu, selection):
+    pass
+
+def setup_search(term):
+    pass
+
+def joke_store(joke):
+    pass
+
+    
+def check_joke(joke):
+    pass
+
+    
+def generate_random_joke():
+    pass
+
+def add_to_database(joke):
+    pass
+def delete_from_database(id):
+    pass
+
+    
+        
+
+
+    
