f9b1ef29a142fd163eeb9e3c3bd8948ea35ca7ae
Darren Isaacson <yc8121yl@go.minneapolis.edu>
2020-03-03 14:29:52 -0600

    Added some basic tests for the view tools.py file

--- configs/db_tools.py
+++ configs/db_tools.py
@@ -1,8 +1,11 @@
 from database.database import *
 
 def add_joke(joke):
-    new_joke = Jokes(joke_text = f'{joke}')
-    return new_joke.save()
+    try:
+        new_joke = Jokes(joke_text = f'{joke}')
+        return new_joke.save()
+    except:
+        print('There was a error')
 def delete_joke(id):
     return Jokes.delete().where(Jokes.joke_id == id).execute()
 def search_joke_id(id):
