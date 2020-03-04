c2d19da19b833864db1ed84d367c9a39686e4309
Orion <gn9081ca@go.minneapolis.edu>
2020-03-04 12:06:27 -0600

    Darren's work on test_application.py

--- test_application.py
+++ test_application.py
@@ -1,8 +1,44 @@
+import unittest 
 from unittest import TestCase
+from unittest.mock import patch, call
+
+import ui.ui as ui
+import configs.db_tools as db_tools
 import configs.view_tools as view_tools
+import database.database as database
 import view
 
+
+
+
 class TestJokesterApplications(TestCase):
+
+    test_db_url = 'test_jokester_db.sqlite'
+
+    def setup(self):
+
+        database.db = self.test_db_url
+        database.connect_db()
+
+
+    @patch('builtins.input', side_effect=['21','d', -2, 2])
+    def test_input_id_selection_validation(self, mock_input):
+        id = ui.input_selection()
+        self.assertEqual(2, id)
+        self.assertNotEqual(-2,id)
+        self.assertRaises(ValueError)
+    
+    @patch('builtins.input', side_effect=['something'])
+    def test_search_validation(self, mock_input):
+        search = ui.search()
+        self.assertEqual('something', search)
+        self.assertNotEqual('-2,id', search)
+
+    @patch('builtins.print')
+    def test_save_joke_confirmation_validation(self, mock_print):
+        ui.display_joke('Some Joke')
+        mock_print.assert_called_once_with('\nSome Joke\n')
+        
     def test_menu_selection(self):
         menu = view.menu_generator()
         self.assertEqual(view_tools.make_selection(menu, 1), menu[1])
@@ -36,7 +72,8 @@ class TestJokesterApplications(TestCase):
         self.assertIsNotNone(view_tools.generate_random_joke)
 
     
-
+    def tearDown(self):
+        database.db.close()
 
 
 
