3a3e1cfd83db5749972ef4971b0e5e7bb6c57014
Orion <gn9081ca@go.minneapolis.edu>
2020-03-03 23:32:47 -0600

    Sameer Joke Returns a Joke!

--- api/sameer_joke.py
+++ api/sameer_joke.py
@@ -3,11 +3,5 @@ url = 'https://geek-jokes.sameerkumar.website/api'
 
 def generate_sameer_joke():
     response = requests.get(url)
-    joke = response.json()['contents']
-    print(joke)
-    #joke =''
+    joke = response.json() #the whole json file is a joke. No dictionary needed
     return joke
-
-
-
-generate_sameer_joke()
